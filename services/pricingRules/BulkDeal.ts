9d6bf21ca68e67f3f2ee4871f314a33c00809ec4
gourab1996 <gourab1996@outlook.com>
2024-02-26 18:10:39 +0530

    BulkDeal added

---
+++ services/pricingRules/BulkDeal.ts
@@ -0,0 +1,19 @@
+// rules/XForYDeal.ts
+
+import { IPricingRule } from '../../interface/pricingRules';
+import { Product } from '../../models/product';
+
+export class BulkDeal implements IPricingRule {
+  constructor(private sku: string, private threshold: number, private pricePerItem: number) {}
+
+  apply(items: Product[]): number {
+    let totalDiscount = 0;
+    const applicableItems = items.filter(item => item.sku === this.sku);
+    if (applicableItems.length > this.threshold) {
+        applicableItems.forEach(item => {
+            totalDiscount += item?.price - this.pricePerItem;
+        });
+    }
+    return totalDiscount;
+  }
+}
